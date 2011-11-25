3ace140e7c76cd7c67988ccbd259c4706d7b2035
Marius Hein <marius.hein@netways.de>
2011-11-25 11:31:56 +0100

    * Cleaning up JS code (ref #2120) WIP
    Conflicts:
    
    	app/modules/AppKit/lib/js/AppKitUtil.js
    	app/modules/AppKit/lib/js/admin/UserManager.js
    	app/modules/AppKit/lib/js/admin/components/CVGridPanel.js
    	app/modules/AppKit/lib/js/admin/components/CredentialGrid.js
    	app/modules/AppKit/lib/js/admin/components/GroupRestrictionView.js
    	app/modules/AppKit/lib/js/admin/components/RestrictionFlagsView.js
    	app/modules/AppKit/lib/js/admin/components/RoleInheritanceView.js
    	app/modules/AppKit/lib/js/admin/components/UserListingGrid.js
    	app/modules/AppKit/lib/js/admin/components/UserSelectionGrid.js
    	app/modules/Cronks/lib/js/Cronks/Tackle/Cronk.js

--- app/modules/Cronks/lib/js/grid/IcingaGridFilterHandler.js
+++ app/modules/Cronks/lib/js/grid/IcingaGridFilterHandler.js
@@ -321,7 +321,9 @@ Cronk.util.GridFilterWindow = function() {
 	
 				// Objects				
 				oWindow().destroy();
-				oCoPanel && oCoPanel.destroy();
+				if (oCoPanel) {
+					oCoPanel.destroy();
+				}
 				
 				// Data
 				oRestrictions = {};
